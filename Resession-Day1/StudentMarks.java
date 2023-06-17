2bc58418657e2c456b734d1a1345cae7af85b470
vamsi gubbala <vamsigubbala7997@gmail.com>
2023-06-17 19:00:41 +0530

    day1

---
+++ Resession-Day1/StudentMarks.java
@@ -0,0 +1,14 @@
+public class StudentMarks {
+    public static void main(String[] args) {
+
+        int a = 80 ;
+        int b = 85;
+        int c = 77;
+
+
+        int R = a + b + c;
+        System.out.println("Total Marks: " + R);
+        double avg = R / 3;
+        System.out.println("Average Marks: " + avg);
+    }
+}
